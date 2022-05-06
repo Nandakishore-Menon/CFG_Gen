ee2afde8205242d9c07fb9e61f980c19a8b001f1
Yashovardhan08 <yashovardhan08@pop-os.localdomain>
2022-05-06 10:08:59 +0530

    added parser and lexical analyser

---
+++ test/parser/test_c_file.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+    int a=0;
+    long d=0;
+    while(a<10){
+        a++;
+        while(d<10)d++;
+    }
+
+    if(a>10){
+        if(a>20)a++;
+        else if(a<13)a;
+        else {a;}
+    }
+    else if(a){a;}
+    else a;
+
+    return 0;
+}
