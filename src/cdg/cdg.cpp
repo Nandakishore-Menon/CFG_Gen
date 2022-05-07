c5d889e487a3b3551bc0da740ab2b0ced19ff71d
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2022-05-07 14:16:26 +0530

    conditionals cdg bug fixed

--- src/cdg/cdg.cpp
+++ src/cdg/cdg.cpp
@@ -7,36 +7,8 @@ CDG::CDG(Node* lines) {
     this->lines = lines;
     generate(NULL, this->lines, true);
     printListRec(this->lines);
-    /*
-    Node* current_line = this->lines;
-    while(current_line != NULL) {
-        switch (current_line->line_type)
-        {
-        case BLOCK:
-            // Next will be the next line itself
-            break;
-        
-        case IF:
-        case ELSEIF:
-        case ELSE:
-        case FOR:
-        case WHILE:
-            Node* line_child = current_line->child;
-            line_child = current_line->next;
-            line_child->parent = current_line;
-            if (!current_line->multiline) {
-                current_line->next = line_child->next;
-                line_child->next = NULL;
-            }
-            break;
-        case CLOSEBRACE:
-
-        default:
-            break;
-        }
-    }
-    */
 }
+
 Node* CDG::generate(Node* prev, Node* current, bool multiline_block) {
 /*bool multiline_block indicates if the current line is part of a multiline block.
 Returns the next line after the block that satrts with the current line
@@ -67,51 +39,62 @@ Returns the next line after the block that satrts with the current line
         current_line->child = line_child;
         line_child->parent = current_line;
         Node* line_next = generate(current, line_child, current_line->multiline);
+        // std::cout << "Current line: ..." << *current_line;
+        // std::cout << "Possible next line: ..." << *line_next << std::endl; 
         if (multiline_block || line_next->line_type == ELSEIF || line_next->line_type == ELSE) {
             // Then this block is not the last in the current block
             // Next block will belong to the current block itself
             current_line->next = line_next;
             line_next->prev = current_line;
+            return generate(current, line_next, multiline_block);
         }
         else {
             // Then this block is the last in the current block
             // Next block will belong to the outer blocks
             current_line->next = NULL;
-        }
-        if (!current_line->multiline)
             return line_next;
-        return generate(current, line_next, multiline_block);
+        }
         break;
     }
     case ELSE:
     case FOR:
     case WHILE:
+    case SWITCH: //multiline always true
+    case CASE:
     {
         // std::cout<< "--------------Going into conditionals\n";
         Node* line_child = current_line->next;
         current_line->child = line_child;
         line_child->parent = current_line;
         Node* line_next = generate(current, line_child, current_line->multiline);
+        // std::cout << "Current line: ..." << *current_line;
+        // std::cout << "Possible next line: ..." << *line_next << std::endl; 
         if (multiline_block) {
             // Then this block is not the last in the current block
             // Next block will belong to the current block itself
             current_line->next = line_next;
             line_next->prev = current_line;
+            return generate(current, line_next, multiline_block);
         }
         else {
             // Then this block is the last in the current block
             // Next block will belong to the outer blocks
             current_line->next = NULL;
-        }
-        if (!current_line->multiline)
             return line_next;
-        return generate(current, line_next, multiline_block);
+        }
         break;
     }
+    // case SWITCH:
+    // {
+    //     Node* line_child = current_line->next;
+    //     current_line->child = line_child;
+    //     line_child->parent = current_line;
+    // }
     case CLOSEBRACE:
     {
         Node* line_next = current_line->next;
         current_line->next = NULL;
+        // std::cout<<"-----"<< *(current_line->prev) <<"---closing returning..." << *line_next;
         return line_next;
         break;
     }
--- test/cdg/cdg_test.cpp
+++ test/cdg/cdg_test.cpp
@@ -4,17 +4,23 @@
 
 Node* generate_sample_code1();
 Node* generate_sample_code2();
+Node* generate_sample_code3();
 
 int main() {
-    /*
+    
     std::cout << "Test case 1:--------------------\n";
     Node* lines1 = generate_sample_code1();
     CDG* cdg1 = new CDG(lines1);
     std::cout << "--------------------------------\n";
-    */
+    
     std::cout << "Test case 2:--------------------\n";
     Node* lines2 = generate_sample_code2();
     CDG* cdg2 = new CDG(lines2);
+    std::cout << "--------------------------------\n";
+    
+    std::cout << "Test case 3:--------------------\n";
+    Node* lines3 = generate_sample_code3();
+    CDG* cdg3 = new CDG(lines3);
     std::cout << "--------------------------------\n";
 }
 
@@ -78,9 +84,50 @@ Node* generate_sample_code2() {
     append(&lines, new_node);
     new_node = new Node(8, "else", ELSE, false);
     append(&lines, new_node);
-    new_node = new Node(9, "int x = 5;", EXPRESSION, false);
+    new_node = new Node(9, "x = 5;", EXPRESSION, false);
     append(&lines, new_node);
     new_node = new Node(10, "x++;", EXPRESSION, false);
+    append(&lines, new_node);
+    return lines;
+}
+
+Node* generate_sample_code3() {
+/*Sample code:
+    int x = 0, y=0 ;
+    if (x<1) 
+        if (x<3) {
+            if (x<7)
+                if (x<10)
+                    while (x<10) {
+                        x++;
+                    }
+            y++;
+        }
+    x = y;
+    */
+    // Linked list after parsing for the above sample code:
+    Node* lines = NULL;
+    Node* new_node = new Node(1, "int x = 0, y=0", EXPRESSION, false);
+    append(&lines, new_node);
+    new_node = new Node(2, "if (x<1)", IF, false);
+    append(&lines, new_node);
+    new_node = new Node(3, "if (x<3) {", IF, true);
+    append(&lines, new_node);
+    new_node = new Node(4, "if (x<7)", IF, false);
+    append(&lines, new_node);
+    new_node = new Node(5, "if (x<10)", IF, false);
+    append(&lines, new_node);
+    new_node = new Node(6, "while (x<10) {", WHILE, true);
+    append(&lines, new_node);
+    new_node = new Node(7, "x++", EXPRESSION, false);
+    append(&lines, new_node);
+    new_node = new Node(8, "}", CLOSEBRACE, false);
+    append(&lines, new_node);
+    new_node = new Node(9, "y++;", EXPRESSION, false);
+    append(&lines, new_node);
+    new_node = new Node(10, "}", CLOSEBRACE, false);
+    append(&lines, new_node);
+    new_node = new Node(11, "x=y;", EXPRESSION, false);
     append(&lines, new_node);
     return lines;
 }
