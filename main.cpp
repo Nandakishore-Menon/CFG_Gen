90a55f306e386a3f52771a1030f081e8bc499710
RachnaKedigehalli <rachna.kedigehalli@iiitb.org>
2022-05-09 20:47:06 +0530

    lexer-Yash

--- main.cpp
+++ main.cpp
@@ -3,10 +3,33 @@
 #include "./include/node.hpp"
 #include "./include/linked_list/list_ops.hpp"
 #include "./include/reader/ll_generator.hpp"
+#include "./include/cdg/cdg.hpp"
+#include "./include/cfg/cfg.hpp"
+
+void generate_CFG(Node* root, std::string func_name);
 
 int main(void) {
     
     Node* root = generate_linked_list();
     std::cout << "Generated licked list-----------\n";
     printListRec(root);
+
+    // generate_CFG(root->next, root->code);
 }
+
+/*
+void generate_CFG(Node* root, std::string func_name) {
+    CDG* cdg = new CDG(root);
+    CFG* cfg = new CFG(cdg->lines);
+    // cfg->CFG_gen();
+    std::cout << "\n--------------CFG-----------------\n";
+    std::set<int> st;
+    std::fstream fio;
+    fio.open(func_name, std::ios::trunc | std::ios::out | std::ios::in);
+    std::set<int> file_stack;
+    // fio << "digraph example {\n";
+    // test1->createDot(test1->cdg, st, file_stack, fio);
+    // fio << "}";
+    fio.close();
+}
+*/
--- test_c_file.c
+++ test_c_file.c
@@ -3,7 +3,7 @@
 long long main(void){
     int a=0;
     long d=0;
-    while(a<10){
+    while(a/*dwq*/</*qsx*/10){
         a++;
         while(d<10)d++;
     }
@@ -17,4 +17,104 @@ long long main(void){
     else a;
 
     return 0;
+}
+void func1(ac,cas) {
+    int x = 0 ;
+    int y = 2 ;
+    if ( x < 5 ) {
+        x = y ;
+        y += 1 ;
+    }
+    x++;
+}
+
+void func2() {
+    int x = 0 ;
+    int y = 2 ;
+    for (int i = 0;/*dwsh*/ i < 10; i++)
+        if (x < 5) {
+            x = y ;
+            y += 1 ;
+        }
+        else
+            x = 5 ;
+    x++;
+}
+
+void func3(){
+    int x = 0, y=0 ;
+    if (x<1) 
+        if (x<3) {
+            if (x<7)
+                if (x<10)
+                    while (x<10) {
+                        x++;
+                    }
+            y++;
+        }
+    x = y;
+}
+
+void func4(  )  
+{
+    int x = 0, y=0 ;
+    if (x<1) 
+        if (x<3) {
+            if (x<7)
+                if (x<10)
+                    while (x<10) {
+                        x++;
+                    }
+                else
+                    x--;
+            else
+                y--;
+            y++;
+        }
+    x = y;
+}
+
+void func5() {
+    int x=0;
+    switch(x) 
+    {
+        case 0:
+            x++;
+        case 1:
+        case 2:
+            x++;
+            x--;
+        default:
+            break;
+    }
+}
+
+void func6() {
+    int x=0;
+    switch(x) {
+        case 1:
+            x+=1;
+        case 2:
+            x+= 2;
+            break;
+        case 3:
+            x+=3;
+        default:
+            x+=x;
+    }
+    x++;
+}
+
+void func7() {
+    int x=0;
+    for(int i = 0 ; i < 10; i++){
+        switch(x) {
+            case 1:
+            case 2:
+            case 3:
+                x+=3;
+                break;
+            default:
+        }
+    }
 }
